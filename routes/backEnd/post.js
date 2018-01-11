a90464015e3e904438268031bcecf552e376583f
吴金锋 <jinfeng.wu@edaibu.net>
2018-01-11 14:33:03 +0800

    作者查询优化

--- routes/backEnd/post.js
+++ routes/backEnd/post.js
@@ -16,49 +16,45 @@ router.get('/list', function (req, res) {
     let createdAt = req.query.createdAt;
     let title = req.query.title;
     let status = req.query.status;
+    let username = req.query.author;
     let postcategoryId = req.query.postcategoryId;
     let currentPage = req.query.currentPage ? (req.query.currentPage - 1) : 0;
     let limit = Number(req.query.pageSize) || 10;
     let skip = currentPage * limit;
     let params = {};
-    if(createdAt){
+    if (createdAt) {
         params.createdAt = {$gte: createdAt[0], $lt: createdAt[1]};
     }
-    if(title){
+    if (title) {
         let regex = new RegExp(title, 'i');
         params.title = regex;
     }
-    if(status){
+    if (status) {
         params.status = status;
     }
-    if(postcategoryId){
+    if (postcategoryId) {
         params.postcategoryId = postcategoryId;
     }
-    mongo.PostModel.find(params)
-        .select({title:1,author:1,createdAt:1,status:1,postcategoryId:1})
-        .skip(skip).limit(limit)
-        .sort({'sort':-1,'updatedAt': -1})
-        .populate({path:'author',select:'username'})
-        .populate({path:'postcategoryId',select:'name'})
-        .lean()
-        .exec(function (err, docs) {
-        if (err) {
-            logger.error(err);
-            return res.json({code: 200, msg: '', data: []});
-        }
-        mongo.PostModel.find(params).count().exec(function (err, totalNum) {
+    if (username) {
+        mongo.UserModel.find({username: new RegExp(username, 'i')}).select({_id:1}).lean().exec(function (err, docs) {
             if (err) {
                 logger.error(err);
-                return res.json({code: 500, msg: err});
+                return res.json({code: 500, msg: err, data: []});
             }
-            docs = docs.map(function (obj) {
-                obj.createdAt = moment(obj.createdAt).format('YYYY-MM-DD HH:mm:ss');
-                return obj
-            });
+            console.log('docs')
             console.log(docs)
-            res.json({code: 200, msg: '', data: {tableData: docs, totalNum: totalNum}});
+            if (docs.length) {
+                params.author = docs.map(function (o) {
+                    return o._id;
+                });
+                findPostList(res, params, skip, limit)
+            }else{
+                return res.json({code: 200, msg: '', data: []});
+            }
         });
-    });
+    }else{
+        findPostList(res, params, skip, limit);
+    }
 });
 
 // 获取：一篇文章 /post/get/id
@@ -133,5 +129,32 @@ function postData(body, author) {
     return data
 }
 
+function findPostList(res, params, skip, limit) {
+    mongo.PostModel.find(params)
+        .select({title: 1, author: 1, createdAt: 1, status: 1, postcategoryId: 1})
+        .skip(skip).limit(limit)
+        .sort({'sort': -1, 'updatedAt': -1})
+        .populate({path: 'author', select: 'username'})
+        .populate({path: 'postcategoryId', select: 'name'})
+        .lean()
+        .exec(function (err, docs) {
+            if (err) {
+                logger.error(err);
+                return res.json({code: 500, msg: err, data: []});
+            }
+            docs = docs.map(function (obj) {
+                obj.createdAt = moment(obj.createdAt).format('YYYY-MM-DD HH:mm:ss');
+                return obj
+            });
+            console.log(docs)
+            mongo.PostModel.find(params).count().exec(function (err, totalNum) {
+                if (err) {
+                    logger.error(err);
+                    return res.json({code: 500, msg: err});
+                }
+                res.json({code: 200, msg: '', data: {tableData: docs, totalNum: totalNum}});
+            });
+        });
+}
 
 module.exports = router;
